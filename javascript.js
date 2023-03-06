17bd3a822d0b1523663bfe5e357b862d7517014f
aspen <jgromadzinska9@gmail.com>
2023-03-06 20:35:10 +0000

    Add hover effect

--- javascript.js
+++ javascript.js
@@ -4,14 +4,28 @@ let gridRow = 16;
 
 let gridSize = gridRow * gridRow;
 
+let pixel;
+
+//let pixels;
+
+
+
 function createGrid() {
     for (let i = 0; i < gridSize; i++) {
-        const pixel = document.createElement('div')
+        let pixel = document.createElement('div');
         pixel.classList.add('pixel')
         pixel.style.width = `${400 / gridRow}px`;
         pixel.style.height = `${400 / gridRow}px`;
         container.appendChild(pixel);
-    }
+        pixel.addEventListener('mouseover', () =>{
+            pixel.style.backgroundColor = 'black';
+        });
+        
+    };
 }
 
 createGrid();
+
+function hoverEffect() {
+    pixel.style.backgroundColor = 'black';
+}
